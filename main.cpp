13f5ff071d71aedd4564b0b42bfdf8c893bd05e4
Henrik Vik <henkvik@gmail.com>
2016-11-22 21:11:42 +0100

    EVERYTHING

---
+++ main.cpp
@@ -0,0 +1,202 @@
+#include <SDL.h>
+#include <GL/glew.h>
+#include <fstream>
+
+void GL_Die(const char *errorMessage);
+void SDL_Die(const char *errorMessage);
+void SDL_GL_SetAttributes();
+SDL_Window* createWindow(const char *name, int width, int height);
+void handleWindowEvents(SDL_Window *window);
+
+GLuint loadShader(GLenum type, const char *fileName);
+void catchShaderError(GLuint shader);
+GLuint createShaderProgram();
+
+
+int main(int argc, char ** argv)
+{
+	// Create the window
+	SDL_Window *mainWindow = createWindow("DV1541 OpenGL SDL", 640, 480);
+
+	// Set OpenGL Settings
+	SDL_GL_SetAttributes();
+	SDL_GL_SetSwapInterval(1);
+
+	// Initialize Glew
+	glewInit();
+	glViewport(0, 0, 640, 480);
+
+	// Set "empty" color of the window
+	glClearColor(0.f, 0.f, 0.f, 1.f);
+
+	// Compile and link shaders into program
+	GLuint shaderProgram = createShaderProgram();
+	GLuint vertexArray = 0;
+
+	while (true)
+	{
+		// Take care of window events
+		handleWindowEvents(mainWindow);
+
+		// Clear the back buffer
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Use the shaders
+		glUseProgram(shaderProgram);
+
+		// Draw the box on the back buffer
+		glBindVertexArray(vertexArray);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+		// Swap the render buffers to show changes in the window.
+		SDL_GL_SwapWindow(mainWindow);
+	}
+
+	return 0;
+}
+
+void GL_Die(const char *errorMessage)
+{
+	// Create a message box to show the error
+	SDL_ShowSimpleMessageBox(
+		SDL_MESSAGEBOX_ERROR,
+		"GL Died",
+		errorMessage,
+		NULL);
+
+	// Exit application
+	SDL_Quit();
+	exit(-1);
+}
+
+void SDL_Die(const char *errorMessage)
+{
+	// Create a message box to show the error
+	SDL_ShowSimpleMessageBox(
+		SDL_MESSAGEBOX_ERROR,
+		errorMessage,
+		SDL_GetError(),
+		NULL);
+
+	// Exit application
+	SDL_Quit();
+	exit(-1);
+}
+
+void SDL_GL_SetAttributes()
+{
+	// Set the active OpenGL version to 3.2
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+	// Make sure we can only use the newest version of the api
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	// Set the render mode to doubble buffer
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+}
+
+SDL_Window* createWindow(const char *name, int width, int height)
+{
+	SDL_Window *window;
+
+	// Try to initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		SDL_Die("Unable to initialize SDL");
+
+	// Create an SDL Window
+	window = SDL_CreateWindow(
+		name,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		width,
+		height,
+		SDL_WINDOW_OPENGL);
+
+	// Check if creation was successful
+	if (!window)
+		SDL_Die("Failed to create Window");
+
+	// Try to create a GL Context in window
+	if (!SDL_GL_CreateContext(window))
+		SDL_Die("Failed to create GL Context");
+
+	return window;
+}
+
+GLuint loadShader(GLenum type, const char * fileName)
+{
+	// Register a new shader
+	GLuint shader = glCreateShader(type);
+
+	// Open shader file and save its content in a string
+	std::ifstream file(fileName);
+	std::string fileContent(
+		(std::istreambuf_iterator<char>(file)),
+		std::istreambuf_iterator<char>());
+	file.close();
+
+	// Make a pointer to a cstring
+	const char* fileContentPtr = fileContent.c_str();
+
+	// Load shader code into graphics card and compile it
+	glShaderSource(shader, 1, &fileContentPtr, nullptr);
+	glCompileShader(shader);
+	
+	// Check if any erros occured and handle them
+	catchShaderError(shader);
+
+	return GLuint();
+}
+
+void catchShaderError(GLuint shader)
+{
+	GLint success = 0;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+	if (success == GL_FALSE)
+	{
+		GLint logSize = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logSize);
+
+		char *log = new char[logSize];
+		glGetShaderInfoLog(shader, logSize, &logSize, &log[0]);
+
+		GL_Die(log);
+	}
+}
+
+GLuint createShaderProgram()
+{
+	GLuint program = 0;
+	// Create vertex and fragment shader
+	GLuint vs = loadShader(GL_VERTEX_SHADER, "VertexShader.glsl");
+	GLuint fs = loadShader(GL_FRAGMENT_SHADER, "FragmentShader.glsl");
+
+	// Link shader program (connect vs and ps)
+	program = glCreateProgram();
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+
+	return program;
+}
+
+void handleWindowEvents(SDL_Window *window)
+{
+	static SDL_Event windowEvent;
+
+	// Poll all events sent to window
+	while (SDL_PollEvent(&windowEvent)) {
+		// Check what event type and handle appropriately
+		switch (windowEvent.type)
+		{
+		case SDL_QUIT:
+			SDL_Quit();
+			exit(0);
+			break;
+
+		}
+	}
+}
+
